efd1977a03ac288d2174aa64da2ed07c81692312
yccjack <568166723@qq.com>
2020-04-12 19:46:09 +0800

    eureka中文

---
+++ src/test/java/com/ycc/eureka/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ycc.eureka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
